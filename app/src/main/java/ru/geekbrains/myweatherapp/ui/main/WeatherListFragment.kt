e02e0f2d20bc2eb85f2f9609d9f96c173bdfee37
Alecsey Efimov <alicsey2000@gmail.com>
2022-10-20 18:24:52 +0300

    Изменения в WeatherListFragment | Добавлена работа с view через viewBinding. Добавлено тело метода обратного вызова для Observer. Добавлено обновление данных для view.

--- app/src/main/java/ru/geekbrains/myweatherapp/ui/main/WeatherListFragment.kt
+++ app/src/main/java/ru/geekbrains/myweatherapp/ui/main/WeatherListFragment.kt
@@ -1,13 +1,16 @@
 package ru.geekbrains.myweatherapp.ui.main
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.snackbar.Snackbar
 import ru.geekbrains.myweatherapp.AppState
 import ru.geekbrains.myweatherapp.R
+import ru.geekbrains.myweatherapp.Weather
+import ru.geekbrains.myweatherapp.databinding.MainFragmentBinding
 
 class WeatherListFragment : Fragment() {
 
@@ -16,23 +19,54 @@ class WeatherListFragment : Fragment() {
     }
 
     private lateinit var viewModel: WeatherListViewModel
+    private lateinit var binding: MainFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        binding = MainFragmentBinding.inflate(inflater)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(this).get(WeatherListViewModel::class.java)
-        viewModel.getLiveData().observe(viewLifecycleOwner) { renderData(it)}
+        viewModel.getLiveData().observe(viewLifecycleOwner) { renderData(it) }
 
+        viewModel.getWeather()
     }
 
-    private fun renderData(data: AppState) {
+    private fun renderData(appState: AppState) {
+        when (appState) {
+            is AppState.Success -> {
+                val weatherData = appState.weatherData
+                binding.loadingLayout.visibility = View.GONE
+                setData(weatherData)
+            }
+            is AppState.Loading -> {
+                binding.loadingLayout.visibility = View.VISIBLE
+            }
+            is AppState.Error -> {
+                binding.loadingLayout.visibility = View.GONE
+                Snackbar
+                    .make(binding.mainView, "Error", Snackbar.LENGTH_INDEFINITE)
+                    .setAction("Reload") { viewModel.getWeather() }
+                    .show()
+            }
 
+        }
+    }
+
+    private fun setData(weatherData: Weather) {
+        binding.cityName.text = weatherData.city.name
+        binding.cityCoordinates.text = String.format(
+            getString(R.string.city_coordinates),
+            weatherData.city.lat.toString(),
+            weatherData.city.lon.toString()
+        )
+        binding.temperatureValue.text = weatherData.temperature.toString()
+        binding.feelsLikeValue.text = weatherData.feelsLike.toString()
     }
 
 }
