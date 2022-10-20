96a59a9d4be1318ccba6c3ab373d54a8727065f5
Alecsey Efimov <alicsey2000@gmail.com>
2022-10-20 14:18:09 +0300

    Изменение ViewModel | Добавлено свойство типа MutableLiveData и геттер getLiveData для него.

--- app/src/main/java/ru/geekbrains/myweatherapp/ui/main/WeatherListViewModel.kt
+++ app/src/main/java/ru/geekbrains/myweatherapp/ui/main/WeatherListViewModel.kt
@@ -1,7 +1,9 @@
 package ru.geekbrains.myweatherapp.ui.main
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class WeatherListViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class WeatherListViewModel(private val liveData: MutableLiveData<Any> = MutableLiveData<Any>()) : ViewModel() {
+    fun getLiveData() = liveData
+
 }
