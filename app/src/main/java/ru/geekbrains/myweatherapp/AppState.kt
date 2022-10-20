aee4d2d07d63acb290f99a6dfac29ae79464cca4
Alecsey Efimov <alicsey2000@gmail.com>
2022-10-20 15:50:56 +0300

    Замена Any на Weather в AppState.Success

--- app/src/main/java/ru/geekbrains/myweatherapp/AppState.kt
+++ app/src/main/java/ru/geekbrains/myweatherapp/AppState.kt
@@ -1,7 +1,7 @@
 package ru.geekbrains.myweatherapp
 
 sealed class AppState {
-    data class Success(val weatherData: Any) : AppState()
+    data class Success(val weatherData: Weather) : AppState()
     data class Error(val error: Throwable) : AppState()
     object Loading : AppState()
 }
