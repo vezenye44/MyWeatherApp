f6b5fee5a80710a1699748c26a0cae6086efc3dd
Alecsey Efimov <alicsey2000@gmail.com>
2022-12-20 15:41:11 +0300

    Добавление фрагмента для отображения списка контактов ContentProviderFragment.kt. Обработка нажатия на новый элемент в меню MainActivity.kt -> Создание ContentProviderFragment.kt.

--- app/src/main/java/ru/geekbrains/myweatherapp/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/myweatherapp/MainActivity.kt
@@ -10,12 +10,13 @@ import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import ru.geekbrains.myweatherapp.ui.contentprovider.ContentProviderFragment
 import ru.geekbrains.myweatherapp.ui.history.HistoryFragment
 import ru.geekbrains.myweatherapp.ui.listweather.WeatherListFragment
 
 class MainActivity : AppCompatActivity() {
 
-    private val receiver : BroadcastReceiver = object : BroadcastReceiver(){
+    private val receiver: BroadcastReceiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
             Toast.makeText(this@MainActivity, "Изменение связи", Toast.LENGTH_LONG).show()
         }
@@ -37,8 +38,18 @@ class MainActivity : AppCompatActivity() {
         menuInflater.inflate(R.menu.main_screen_menu, menu)
         return super.onCreateOptionsMenu(menu)
     }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
+            R.id.menu_content_provider -> {
+                supportFragmentManager.apply {
+                    beginTransaction()
+                        .add(R.id.container, ContentProviderFragment.newInstance())
+                        .addToBackStack("")
+                        .commitAllowingStateLoss()
+                }
+                true
+            }
             R.id.menu_history -> {
                 supportFragmentManager.apply {
                     beginTransaction()
---
+++ app/src/main/java/ru/geekbrains/myweatherapp/ui/contentprovider/ContentProviderFragment.kt
@@ -0,0 +1,36 @@
+package ru.geekbrains.myweatherapp.ui.contentprovider
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ru.geekbrains.myweatherapp.databinding.FragmentContentProviderBinding
+
+class ContentProviderFragment : Fragment() {
+
+    private var _binding: FragmentContentProviderBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentContentProviderBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = ContentProviderFragment()
+    }
+}
