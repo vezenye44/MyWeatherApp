9b372bf7651a0e39052b5723391cc6668ee590a4
Alecsey Efimov <alicsey2000@gmail.com>
2022-12-20 16:41:07 +0300

    Отправляем запрос на доступ к контактам и обрабатываем ответ пользователя.

--- app/src/main/java/ru/geekbrains/myweatherapp/ui/contentprovider/ContentProviderFragment.kt
+++ app/src/main/java/ru/geekbrains/myweatherapp/ui/contentprovider/ContentProviderFragment.kt
@@ -1,9 +1,13 @@
 package ru.geekbrains.myweatherapp.ui.contentprovider
 
+import android.Manifest
+import android.app.AlertDialog
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import ru.geekbrains.myweatherapp.databinding.FragmentContentProviderBinding
 
@@ -17,11 +21,73 @@ class ContentProviderFragment : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentContentProviderBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        checkPermission()
+    }
+
+    private fun checkPermission() {
+        context?.let {
+            when {
+                ContextCompat.checkSelfPermission(
+                    it,
+                    Manifest.permission.READ_CONTACTS
+                ) == PackageManager.PERMISSION_GRANTED -> {
+                    //Доступ к контактам на телефоне есть
+                    //TODO : fun getContacts()
+                }
+                //Опционально: если нужно пояснение перед запросом разрешений
+                shouldShowRequestPermissionRationale(Manifest.permission.READ_CONTACTS) -> {
+                    AlertDialog.Builder(it).setTitle("Доступ к контактам").setMessage("Объяснение")
+                        .setPositiveButton("Предоставить доступ") { _, _ ->
+                            requestPermission()
+                        }.setNegativeButton("Не надо") { dialog, _ ->
+                            dialog.dismiss()
+                        }.create().show()
+                }
+                else -> {
+                    //Запрашиваем разрешение
+                    requestPermission()
+                }
+            }
+        }
+
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when (requestCode) {
+            REQUEST_CODE -> {
+                // Проверяем, дано ли пользователем разрешение по нашему запросу
+                if ((grantResults.isNotEmpty() &&
+                            grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                ) {
+                    // getContacts()
+                } else {
+                            // Поясните пользователю, что экран останется пустым,
+                            // потому что доступ к контактам не предоставлен
+                            context?.let {
+                                AlertDialog.Builder(it)
+                                    .setTitle("Доступ к контактам")
+                                    .setMessage("Объяснение")
+                                    .setNegativeButton("Закрыть") { dialog, _ ->
+                                        dialog.dismiss() }
+                                    .create()
+                                    .show()
+                            }
+                }
+                return
+            }
+        }
+    }
+
+    private fun requestPermission() {
+        requestPermissions(arrayOf(Manifest.permission.READ_CONTACTS), REQUEST_CODE)
     }
 
     override fun onDestroyView() {
@@ -32,5 +98,6 @@ class ContentProviderFragment : Fragment() {
     companion object {
         @JvmStatic
         fun newInstance() = ContentProviderFragment()
+        const val REQUEST_CODE = 42
     }
 }
