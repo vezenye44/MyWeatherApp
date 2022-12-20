87328a208833a68878738b3e3d3545d6a236659a
Alecsey Efimov <alicsey2000@gmail.com>
2022-12-20 17:46:35 +0300

    Получаем и выводим список контактов на экране.

--- app/src/main/java/ru/geekbrains/myweatherapp/ui/contentprovider/ContentProviderFragment.kt
+++ app/src/main/java/ru/geekbrains/myweatherapp/ui/contentprovider/ContentProviderFragment.kt
@@ -2,13 +2,19 @@ package ru.geekbrains.myweatherapp.ui.contentprovider
 
 import android.Manifest
 import android.app.AlertDialog
+import android.content.ContentResolver
+import android.content.Context
 import android.content.pm.PackageManager
+import android.database.Cursor
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.AppCompatTextView
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import ru.geekbrains.myweatherapp.R
 import ru.geekbrains.myweatherapp.databinding.FragmentContentProviderBinding
 
 class ContentProviderFragment : Fragment() {
@@ -40,7 +46,7 @@ class ContentProviderFragment : Fragment() {
                     Manifest.permission.READ_CONTACTS
                 ) == PackageManager.PERMISSION_GRANTED -> {
                     //Доступ к контактам на телефоне есть
-                    //TODO : fun getContacts()
+                    getContacts()
                 }
                 //Опционально: если нужно пояснение перед запросом разрешений
                 shouldShowRequestPermissionRationale(Manifest.permission.READ_CONTACTS) -> {
@@ -67,19 +73,20 @@ class ContentProviderFragment : Fragment() {
                 if ((grantResults.isNotEmpty() &&
                             grantResults[0] == PackageManager.PERMISSION_GRANTED)
                 ) {
-                    // getContacts()
+                    getContacts()
                 } else {
-                            // Поясните пользователю, что экран останется пустым,
-                            // потому что доступ к контактам не предоставлен
-                            context?.let {
-                                AlertDialog.Builder(it)
-                                    .setTitle("Доступ к контактам")
-                                    .setMessage("Объяснение")
-                                    .setNegativeButton("Закрыть") { dialog, _ ->
-                                        dialog.dismiss() }
-                                    .create()
-                                    .show()
+                    // Поясните пользователю, что экран останется пустым,
+                    // потому что доступ к контактам не предоставлен
+                    context?.let {
+                        AlertDialog.Builder(it)
+                            .setTitle("Доступ к контактам")
+                            .setMessage("Объяснение")
+                            .setNegativeButton("Закрыть") { dialog, _ ->
+                                dialog.dismiss()
                             }
+                            .create()
+                            .show()
+                    }
                 }
                 return
             }
@@ -88,6 +95,40 @@ class ContentProviderFragment : Fragment() {
 
     private fun requestPermission() {
         requestPermissions(arrayOf(Manifest.permission.READ_CONTACTS), REQUEST_CODE)
+    }
+
+    private fun getContacts() {
+        context?.let {
+            // Получаем ContentResolver у контекста
+            val contentResolver: ContentResolver = it.contentResolver
+            // Отправляем запрос на получение контактов и получаем ответ в виде Cursor
+            val cursorWithContacts: Cursor? = contentResolver.query(
+                ContactsContract.Contacts.CONTENT_URI,
+                null,
+                null,
+                null,
+                ContactsContract.Contacts.DISPLAY_NAME + " ASC"
+            )
+            cursorWithContacts?.let { cursor ->
+                for (i in 0..cursor.count) {
+                    // Переходим на позицию в Cursor
+                    if (cursor.moveToPosition(i)) {
+                        // Берём из Cursor столбец с именем
+                        val name =
+                            cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME))
+                        addView(it, name)
+                    }
+                }
+            }
+            cursorWithContacts?.close()
+        }
+    }
+
+    private fun addView(context: Context, textToShow: String) {
+        binding.containerForContacts.addView(AppCompatTextView(context).apply {
+            text = textToShow
+            textSize = resources.getDimension(R.dimen.main_container_text_size)
+        })
     }
 
     override fun onDestroyView() {
